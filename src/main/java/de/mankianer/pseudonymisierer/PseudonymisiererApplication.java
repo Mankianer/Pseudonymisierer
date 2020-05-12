29a731b52b5fbd993c7a66b72e6709374ea73348
Mankianer <Marvin.woelk@web.de>
2020-05-12 21:14:30 +0200

    init

---
+++ src/main/java/de/mankianer/pseudonymisierer/PseudonymisiererApplication.java
@@ -0,0 +1,13 @@
+package de.mankianer.pseudonymisierer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PseudonymisiererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PseudonymisiererApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/mankianer/pseudonymisierer/PseudonymisiererApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mankianer.pseudonymisierer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PseudonymisiererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
